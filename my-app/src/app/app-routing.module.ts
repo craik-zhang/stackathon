6cf0a0119f44542532554b6d87c0e5ed2427a69c
craik.zhang <craik.zhang@foxmail.com>
2020-04-26 17:06:12 +0800

    init project

--- my-app/src/app/app-routing.module.ts
+++ my-app/src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SignInComponent } from './pages/sign-in/sign-in.component';
+import { ProductComponent } from './pages/product/product.component';
+import { ProductDetailComponent } from './pages/product-detail/product-detail.component';
+import {SigninGuard} from './guard/signin.guard';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'sign-in', component: SignInComponent },
+  { path: 'products', component: ProductComponent, canActivate: [SigninGuard]},
+  { path: 'product/:id', component: ProductDetailComponent, canActivate: [SigninGuard]}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -1,18 +1,44 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
+
+// import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+/* bootstrap 按需导入，可以使我们最终的打包文件更小 */
+import { NgbAlertModule} from '@ng-bootstrap/ng-bootstrap';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductComponent } from './pages/product/product.component';
+import { ProductDetailComponent } from './pages/product-detail/product-detail.component';
+import { SignInComponent } from './pages/sign-in/sign-in.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { MoneyPipe } from './pipe/money.pipe';
+import { FooterComponent } from './componet/footer/footer.component';
+import { NavbarComponent } from './componet/navbar/navbar.component';
+import { JwtInterceptor } from './interceptor/jwt.interceptor';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent,
+    ProductDetailComponent,
+    NavbarComponent,
+    FooterComponent,
+    SignInComponent,
+    MoneyPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    NgbAlertModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
